b2cbfe69ceefb5df7cb23cdee50eea7a531d941d
Siffi Singh <siffisingh26@gmail.com>
2026-07-14 13:37:11 -0700

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        # DFS: Recursion
+        # move from last element to first
+        memo = {}
+
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            if i == len(nums) - 1:
+                return 0
+            if nums[i] == 0:
+                return 1000000
+
+            res = 1000000
+            end = min(len(nums), i + nums[i] + 1)
+            for j in range(i + 1, end):
+                res = min(res, 1 + dfs(j))
+            memo[i] = res
+            return res
+
+        return dfs(0)
