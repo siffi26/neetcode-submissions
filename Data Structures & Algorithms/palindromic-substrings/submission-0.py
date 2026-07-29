13e948a53ece7f51d14358bec38f9a7702a03750
Siffi Singh <siffisingh26@gmail.com>
2026-07-29 11:21:22 -0700

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+        def expand(left, right):
+            nonlocal count
+
+            # Keep expanding to
+            # Check indexes are valid and characters match
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+        
+        for i in range(len(s)):
+            expand(i, i)
+            expand(i, i+1)
+
+        return count
+        
