4e5169e0afd37e000911ca30974640e65b1d8cb9
Siffi Singh <siffisingh26@gmail.com>
2026-09-17 21:08:39 -0700

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,81 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = [[] for _ in range(numCourses)]
+
+        for course, prerequisite in prerequisites:
+            graph[prerequisite].append(course)
+
+        state = [0] * numCourses
+        # 0 = unvisited, 1 = visiting, 2 = processed
+
+        def dfs(course):
+            if state[course] == 1:
+                return False
+
+            if state[course] == 2:
+                return True
+
+            state[course] = 1
+            for next_course in graph[course]:
+                if not dfs(next_course):
+                    return False
+
+            state[course] = 2
+            return True
+        
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+
+        return True
+
+
+            
+
+            
+
+
+
+
+
+        # # If the directed graph contains a cycle, you cannot finish all courses.
+
+        # graph = [[] for _ in range(numCourses)]
+        # # prerequisite -> course
+        # for course, prerequisite in prerequisites:
+        #     graph[prerequisite].append(course)
+
+        # # 0 = unvisited, 1 = visiting, 2 = processed
+        # state = [0] * numCourses
+
+        # def dfs(course):
+        #     # We returned to a node in the current DFS path
+        #     if state[course] == 1:
+        #         return False
+            
+        #     # This course was already fully checked
+        #     if state[course] == 2:
+        #         return True
+            
+        #     # currently exploring course i, haven't finished checking all of its prerequisites/dependents yet."
+        #     # course changes from 1 to 2 before DFS returns:
+        #     # So 1 is temporary.
+        #     # But 1 has not finished yet. That means:1 → 2 → 1. It comes back to state = 1.
+
+        #     state[course] = 1
+        #     for next_course in graph[course]:
+        #         if not dfs(next_course):
+        #             return False
+
+        #     # Remove from current path; mark fully processed
+        #     state[course] = 2
+        #     return True
+
+        # # The graph may have disconnected components
+        # for course in range(numCourses):
+        #     if not dfs(course):
+        #         return False
+
+        # return True
+
+        
